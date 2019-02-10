f8c21564ebe517a785e180ae334357632ec2a757
batter <wb.wangning02@mesg.corp.netease.com>
2019-02-10 09:30:02 +0800

    init

---
+++ src/main/java/com/batter/code4fun/Code4funApplication.java
@@ -0,0 +1,14 @@
+package com.batter.code4fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code4funApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Code4funApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/batter/code4fun/Code4funApplicationTests.java
@@ -0,0 +1,17 @@
+package com.batter.code4fun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Code4funApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
